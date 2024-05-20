4d1145d3937128c09558bc254d96693cfd77e046
abdelrhmanabdelhaleem <abdelrhmman123@gmail.com>
2024-05-20 23:11:36 +0300

    initalize project

---
+++ src/Components/Context/Auth.jsx
@@ -0,0 +1,30 @@
+import { jwtDecode } from "jwt-decode";
+import { createContext, useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+
+export let AuthContext = createContext("");
+export default function AuthContextProvider(props) {
+  const [user, setuser] = useState(null);
+
+  let saveUserData = () => {
+    let enCodeToken = localStorage.getItem("token");
+    let deCodeToken = jwtDecode(enCodeToken);
+    setuser(deCodeToken);
+  };
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      saveUserData();
+    }
+  }, []);
+  let logOut = () => {
+    localStorage.removeItem("token");
+    setuser(null);
+    return <Navigate to="login" />;
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, logOut, saveUserData }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
